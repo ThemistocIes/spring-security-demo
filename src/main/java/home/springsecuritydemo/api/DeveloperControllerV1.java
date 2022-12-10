bb98861735992d9aca68793d9fdd94fcd3d86e1a
Your Name <captainjackvorobey1@gmail.com>
2022-12-10 22:50:38 +0700

    Initial commit

---
+++ src/main/java/home/springsecuritydemo/api/DeveloperControllerV1.java
@@ -0,0 +1,37 @@
+package home.springsecuritydemo.api;
+
+import home.springsecuritydemo.model.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("api/v1/developers")
+public class DeveloperControllerV1 {
+
+    private List<Developer> developers = Stream.of(
+            new Developer(1L, "Geralt", "Riviiskiy"),
+            new Developer(2L, "John", "Wick"),
+            new Developer(3L, "Yuri", "Mikhaylov")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return developers;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return developers
+                .stream()
+                .filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
